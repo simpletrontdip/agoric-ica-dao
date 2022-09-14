851853be102e4767721a4b864b20726302b0a6f1
simpletron <thongitbk@gmail.com>
2022-09-14 23:35:12 +0700

    Reorganize contract folder

--- contract/deploy/start-dao.js
+++ contract/deploy/start-dao.js
@@ -15,7 +15,7 @@ export default async function deploy(homeP, { bundleSource, pathResolve }) {
   const { zoe, scratch, chainTimerService } = await homeP;
 
   const icaContractBundle = await bundleSource(
-    pathResolve(`../src/ica-dao.js`),
+    pathResolve(`../src/ica-dao/ica-dao.js`),
   );
   const icaContractInstall = await E(zoe).install(icaContractBundle);
 
